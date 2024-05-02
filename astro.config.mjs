69c5e37203be6b880c475698bc0c6c4908a7502f
Florence Wolfe <florencewolfe@pm.me>
2024-05-02 10:05:01 -0400

    Update Astro to v3 and add projects section to resume

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,32 +3,21 @@ import { fileURLToPath } from 'url';
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import mdx from '@astrojs/mdx';
-import partytown from '@astrojs/partytown';
 import compress from 'astro-compress';
 import { SITE } from './src/config.mjs';
 import alpinejs from "@astrojs/alpinejs";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const whenExternalScripts = (items = []) => SITE.googleAnalyticsId ? Array.isArray(items) ? items.map(item => item()) : [items()] : [];
 
-// https://astro.build/config
 export default defineConfig({
   site: SITE.origin,
   base: SITE.basePathname,
   trailingSlash: SITE.trailingSlash ? 'always' : 'never',
   output: 'static',
-  experimental: {
-    assets: true
-  },
   integrations: [tailwind({
     config: {
       applyBaseStyles: false
     }
-  }), sitemap(),  mdx(), ...whenExternalScripts(() => partytown({
-    config: {
-      forward: ['dataLayer.push']
-    }
-  })), compress({
+  }), sitemap(), compress({
     css: true,
     html: {
       removeAttributeQuotes: false
